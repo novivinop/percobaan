a84a13e4637c5aba34cf0a6f3c1f54a03ddb3234
unknown <Ikadhanny>
2023-05-12 20:16:14 +0700

    udah ku update connect nya

--- config.php
+++ config.php
@@ -3,7 +3,8 @@
 $hostname = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'db_beasiswa';
+$dbname = 'db_mahasiswa';
+
 // koneksi ke database dengan definename
 $conn = mysqli_connect($hostname, $username, $password, $dbname);
 
@@ -11,4 +12,14 @@ $conn = mysqli_connect($hostname, $username, $password, $dbname);
 if(!$conn) {
     echo "koneksi gagal" + mysqli_connect_error();
 }
+
+// //test conection
+// try {
+//     $conn = new PDO("mysql:host=$servername;db_mahasiswa=$dbname", $username, $password);
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "Connected successfully";
+// } catch(PDOException $e) {
+//     echo "Connection failed: " . $e->getMessage();
+// }
+
 ?>
